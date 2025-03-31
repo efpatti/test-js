d8b41efa071969efbe543bcfc61ad46eb8b02832
Enzo Patti <enzo.patti@aluno.senai.br>
2025-03-31 08:44:52 -0300

    error message implemented as success

---
+++ index.test.js
@@ -0,0 +1,21 @@
+import assert from "assert";
+import Product from "./product";
+import Service from "./service";
+
+// should throw an error when description is less than 5 characters long
+{
+ const params = { description: "my product", id: 1, price: 1000 };
+
+ const product = new Product({
+  onCreate: () => {},
+  service: new Service(),
+ });
+
+ assert.rejects(
+  () => product.create(params),
+  { message: "Description must be higher than 5" },
+  "It should throw an error with wrong description!"
+ );
+
+ product.create(params);
+}
---
+++ product.js
@@ -0,0 +1,17 @@
+import EventEmitter from "events";
+export default class Product {
+ constructor({ onCreate, service }) {
+  this.service = service;
+  this.service = new EventEmitter();
+  this.source.on("create", onCreate);
+ }
+
+ // data  = { description, id, price }
+ #isValid(data) {}
+
+ async create(data) {
+  this.#isValid(data);
+  const message = await this.service.save(data);
+  return message.toUppercase();
+ }
+}
